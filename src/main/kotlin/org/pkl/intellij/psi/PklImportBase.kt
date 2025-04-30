1af0218770e315ff9fc2fcbc8a2bef6b7d89b320
Daniel Chao <daniel.h.chao@gmail.com>
2025-04-30 08:17:06 -0700

    Remove import/read expression from warning suppression target (#95)
    This fixes a runtime assertion error.
    
    Only import clauses make sense as warning suppression targets;
    the expression forms of these can be targeted by targeting their parent
    (e.g. the property that they are assigned to).

--- src/main/kotlin/org/pkl/intellij/psi/PklImportBase.kt
+++ src/main/kotlin/org/pkl/intellij/psi/PklImportBase.kt
@@ -1,5 +1,5 @@
 /**
- * Copyright © 2024 Apple Inc. and the Pkl project authors. All rights reserved.
+ * Copyright © 2024-2025 Apple Inc. and the Pkl project authors. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,6 +15,6 @@
  */
 package org.pkl.intellij.psi
 
-interface PklImportBase : PklElement, PklSuppressWarningsTarget {
+interface PklImportBase : PklElement {
   val moduleUri: PklModuleUri?
 }
