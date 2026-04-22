43ef89256de133dd4e05fcb39256ac7990fba185
Daniel Chao <daniel.h.chao@gmail.com>
2026-04-22 11:11:02 -0700

    Improve dependency management (#184)
    * Enable Dependabot updates for Gradle
    * Prevent dependencies from resolving dynamic versions
    * Upload dependenecy SBOM to GitHub

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,14 @@ repositories {
   }
 }
 
+configurations {
+  all {
+    resolutionStrategy {
+      failOnDynamicVersions()
+    }
+  }
+}
+
 val isCiBuild = System.getenv("CI") != null
 val isReleaseBuild = System.getProperty("releaseBuild") != null
 if (!isReleaseBuild) {
@@ -75,7 +83,7 @@ dependencies {
     pluginVerifier()
     bundledPlugin("org.intellij.intelliLang")
     bundledPlugin("org.intellij.plugins.markdown")
-    testFramework(TestFrameworkType.Platform)
+    testFramework(TestFrameworkType.Platform, libs.versions.intellijTestPlatform.get())
   }
 }
 
